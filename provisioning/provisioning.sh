676249d18336ec97c4397d43ec9ef5f2b9224b0f
danielmenezesbr <danielmenezes@gmail.com>
2023-11-28 15:10:00 -0300

    Update provisioning.sh

--- provisioning/provisioning.sh
+++ provisioning/provisioning.sh
@@ -14,6 +14,8 @@ docker exec gitlab gitlab-rails runner /tmp/gitlab-rails-script.rb $TOKEN
 
 docker run -v $(pwd)/provisioning:/tmp/ansible -v /var/run/docker.sock:/var/run/docker.sock --network=gitlab-network willhallonline/ansible:2.9.27-alpine-3.14 /bin/sh -c "truncate -s 0 /etc/ansible/hosts;ansible-playbook /tmp/ansible/gitlab-ansible.yml"
 
+docker rmi -f willhallonline/ansible:2.9.27-alpine-3.14
+
 docker exec gitlab gitlab-rails runner /tmp/gitlab-rails-script-after-ansible.rb
 
 `dirname $0`/gitlab-runner-register.sh
